e6f9f553a9aed6d1cd93c7c2051ba0660960863a
Thomas VB <van.bouwel.thomas@gmail.com>
2016-08-11 17:30:10 +0200

    First Commit

---
+++ Assets/#Project/Scripts/FollowRotation.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowRotation : MonoBehaviour {
+
+    public bool lockX, lockY, lockZ;
+    public Transform _Target;
+
+	// Update is called once per frame
+	void Update () {
+        Vector3 eulerRot = new Vector3(
+            lockX ? transform.rotation.eulerAngles.x : _Target.rotation.eulerAngles.x,
+            lockY ? transform.rotation.eulerAngles.y : _Target.rotation.eulerAngles.y,
+            lockZ ? transform.rotation.eulerAngles.z : _Target.rotation.eulerAngles.z
+        );
+        transform.rotation = Quaternion.Euler(eulerRot);
+	}
+}
---
+++ Assets/#Project/Scripts/PlayerMovement.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using VRStandardAssets.Utils;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMovement : MonoBehaviour {
+
+    public float _Speed = 2f;
+
+    [SerializeField] private VRInput _VRInput;
+    [SerializeField] private Transform _Cam;
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Use this for initialization
+    private void OnEnable()
+    {
+        _VRInput.OnDown += EnableRotation;
+        _VRInput.OnUp += DisableRotation;
+
+        _VRInput.OnPressed += MoveForeward;
+    }
+
+    private void OnDisable()
+    {
+        _VRInput.OnDown -= EnableRotation;
+        _VRInput.OnUp -= DisableRotation;
+
+        _VRInput.OnPressed -= MoveForeward;
+    }
+
+    private void MoveForeward()
+    {
+        Vector3 forwardVector = Vector3.ProjectOnPlane(_Cam.forward, Vector3.up).normalized;
+        transform.Translate(forwardVector * Time.deltaTime * _Speed);
+    }
+
+    private void EnableRotation()
+    {
+        rb.constraints = RigidbodyConstraints.None;
+        rb.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
+    }
+
+    private void DisableRotation()
+    {
+        rb.constraints = RigidbodyConstraints.None;
+        rb.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationY | RigidbodyConstraints.FreezeRotationZ;
+    }
+}
