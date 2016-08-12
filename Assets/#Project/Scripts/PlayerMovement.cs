7bafb6dcc7dd8ee95a480ff666ccc73375ac7a4c
Thomas VB <van.bouwel.thomas@gmail.com>
2016-08-12 23:07:03 +0200

    Modelupdate - correct player controller

--- Assets/#Project/Scripts/PlayerMovement.cs
+++ Assets/#Project/Scripts/PlayerMovement.cs
@@ -19,35 +19,18 @@ public class PlayerMovement : MonoBehaviour {
     // Use this for initialization
     private void OnEnable()
     {
-        _VRInput.OnDown += EnableRotation;
-        _VRInput.OnUp += DisableRotation;
-
         _VRInput.OnPressed += MoveForeward;
     }
 
     private void OnDisable()
     {
-        _VRInput.OnDown -= EnableRotation;
-        _VRInput.OnUp -= DisableRotation;
-
         _VRInput.OnPressed -= MoveForeward;
     }
 
     private void MoveForeward()
     {
-        Vector3 forwardVector = Vector3.ProjectOnPlane(_Cam.forward, Vector3.up).normalized;
-        transform.Translate(forwardVector * Time.deltaTime * _Speed);
-    }
-
-    private void EnableRotation()
-    {
-        rb.constraints = RigidbodyConstraints.None;
-        rb.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
-    }
-
-    private void DisableRotation()
-    {
-        rb.constraints = RigidbodyConstraints.None;
-        rb.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationY | RigidbodyConstraints.FreezeRotationZ;
+        Vector3 forwardVector = Vector3.ProjectOnPlane(_Cam.forward, Vector3.up);
+        Debug.DrawRay(_Cam.position, forwardVector);
+        transform.Translate(_Cam.forward * Time.deltaTime * _Speed, Space.World);
     }
 }
